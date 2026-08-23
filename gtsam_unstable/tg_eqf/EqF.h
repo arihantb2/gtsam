8aa4c080250448119ae941ce198e2f735da2f5ba
Arihant Lunawat <arihantb2@gmail.com>
2026-08-24 07:49:41 +1000

    update default horizontal variance and change virtual bias anchoring

--- gtsam_unstable/tg_eqf/EqF.h
+++ gtsam_unstable/tg_eqf/EqF.h
@@ -56,7 +56,7 @@ class TGEqF : public gtsam::EquivariantFilter<State, TGSymmetry> {
 
   /// Default variance (m^2) given to the pseudo-measured horizontal axes of a
   /// depth update, large enough that the update leaves x and y untouched.
-  static constexpr double kDefaultHorizontalVariance = 1e3;
+  static constexpr double kDefaultHorizontalVariance = 1e6;
 
   /// Initial standard deviation of the virtual bias b_v. b_v has no physical
   /// counterpart to be uncertain about: it starts at zero and the b_v = 0
@@ -211,7 +211,7 @@ class TGEqF : public gtsam::EquivariantFilter<State, TGSymmetry> {
                        const Eigen::MatrixXd& Cstar, const Eigen::VectorXd& z,
                        const Eigen::MatrixXd& R);
 
-  bool anchor_virtual_bias_ = true;
+  bool anchor_virtual_bias_ = false;
 
   // Process noise and measurement noise for the virtual-bias state.
   Covariance3 virtual_bias_R_ = 1e-6 * Covariance3::Identity();
